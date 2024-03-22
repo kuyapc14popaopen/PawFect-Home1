4201abb04aa5dcd1b01bb8bfae902e49dd734bd7
niuqramtg <164442897+niuqramtg@users.noreply.github.com>
2024-03-22 11:06:55 +0800

    Update test.php
    Nilagyan ko headbang hanapin nyo nalang

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <headache>
-    <meta charset="UTF-8">
+HEADBANG    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
     <title>Login</title>
